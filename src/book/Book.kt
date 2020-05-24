cfb9ca2ee8f35a1148c60014009768ee65007b5d
haokai <haokai.union@outlook.com>
2020-05-24 21:38:46 +0800

    class 6 session 7 finish

--- src/book/Book.kt
+++ src/book/Book.kt
@@ -1,12 +1,55 @@
 package book
 
-open class Book(val title: String, val author: String) {
-    private var currentPage = 0
-    open fun readPage() { currentPage++ }
+import java.time.Year
+
+const val MAXTOBORROW = 10
+
+object BaseUrl{
+    const val BASE_URL = "/shelf/"
 }
 
-class EBook(title: String, author: String, var format: String = "text"): Book(title, author)
+open class Book(val title: String, val author: String, val year: Year) {
+    private var currentPage = 0
+    companion object BaseUrl2{
+        const val BASE_URL = "/shelf2/"
+    }
+
+    open fun readPage() { currentPage++ }
+    fun getTitleAndAuthor(): Pair<String, String>{
+        return title to author
+    }
+    fun getTitleAndAuthorAndYear(): Triple<String, String, Year>{
+        return Triple(title, author, year)
+    }
+
+    fun canBorrow(borrowed: Int): Boolean{
+        return (borrowed < MAXTOBORROW)
+    }
+
+    fun printUrl(){
+        println("${BaseUrl.BASE_URL}$title.html")
+        println("${BaseUrl2.BASE_URL}$title.html")
+    }
+}
+
+class EBook(title: String, author: String, year: Year, var format: String = "text"): Book(title, author, year)
 {
     var words = 0
     override fun readPage() {  words += 250 }
+}
+
+fun main(){
+    val book = Book("programer", "haokai", Year.now())
+    val (title, author, year) = book.getTitleAndAuthorAndYear()
+    println("Here is your book $title written by $author in $year")
+
+    val allBooks = setOf("Book1", "Book2", "Hamlet")
+    val library = mapOf("Shakespeare" to allBooks)
+    println(library.any{it.value.contains("Hamlet")})
+
+    val moreBooks = mutableMapOf("haokai" to "Programmer")
+    moreBooks.getOrPut("jiwenjuan", {"Lady"})
+    println(moreBooks)
+
+    book.printUrl()
 }
